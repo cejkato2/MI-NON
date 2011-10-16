d0a6ceae102d8435471dbd79f825d0fe04c3476c
Tomáš Čejka <tomas@kassoft.eu>
2011-10-16 16:22:32 +0200

    conjugate gradient

--- main.cpp
+++ main.cpp
@@ -4,6 +4,98 @@
 
 using namespace std;
 
+void solve_conjugate_gradient(Matrix A, Vector b, double epsilon)
+{
+/*
+ * 1) x0
+ * 2) r0 = b - A*x0
+ *  2.1) rk = A*x0
+ *  2.2) r0 = b - rk
+ * 3) s0 = r0
+ * 4) rk = r0
+ *
+ * cycle:
+ * 5) alpha = (rk * rk) / (s*A*s)
+ *  5.1) al1 = rk * rk
+ *  5.2) r0 = A*s
+ *  5.3) al2 = s * r0
+ *  5.4) alpha = al1 / al2
+ * 6) xk = xk + alpha * s
+ * 7) rk2 = rk - alpha * A * s
+ * 8) alpha = (rk2*rk2) / (rk*rk)
+ * 9) s = rk2 + alpha * s
+ *10) rk = rk2
+ */
+
+  Vector r0;
+  Vector rk;
+  Vector rk2;
+
+  Vector x0;
+  Vector xk;
+  Vector xk2;
+  Vector s;
+
+  double al1, al2, alpha;
+  double bnorm = b.norm();
+
+  std::cout << "Conjugate gradient" << std::endl;
+
+  // 1)
+  xk.setDim(b.getDim());
+  xk.reset(); // alloc xk = (0, ..., 0)
+
+  r0 = A * xk;
+  rk = b - r0;
+  r0.clean();
+  s = rk * 1.0;
+
+  while (true) {
+    // 5)
+    al1 = rk * rk;
+    r0 = A * s;
+    al2 = s * r0;
+    r0.clean();
+    alpha = al1 / al2;
+
+    // 6) xk2
+    x0 = s * alpha;
+    xk2 = x0 + xk;
+    x0.clean();
+    xk.clean();
+    xk = xk2;
+
+    // 7) rk2
+    rk2 = A * s;
+    r0 = rk2 * alpha;
+    rk2.clean();
+    rk2 = rk - r0;
+    r0.clean();
+   
+    // 8) beta
+    al1 = rk2 * rk2;
+    al2 = rk * rk;
+    alpha = al1 / al2;
+
+    // 9) s2
+    r0 = s * alpha;
+    s.clean();
+    s = rk2 + r0;
+    r0.clean();
+
+    rk.clean();
+    rk = rk2;
+    cout << "Iterate Result: " << xk << " residuum: " << rk << endl;
+    if ((rk.norm() / bnorm) < epsilon) {
+      break;
+    }
+  }
+
+  cout << "Result: " << xk << " residuum: " << rk << endl;
+  xk.clean();
+  rk.clean();
+}
+
 void solve_steepest_descend(Matrix A, Vector b, double epsilon)
 {
 /*
@@ -36,6 +128,8 @@ void solve_steepest_descend(Matrix A, Vector b, double epsilon)
   double al1, al2, alpha;
   double bnorm = b.norm();
 
+  std::cout << "Steepest descend" << std::endl;
+  
   // 1)
   xk.setDim(b.getDim());
   xk.reset(); // alloc xk = (0, ..., 0)
@@ -121,6 +215,7 @@ int main(int argc, char **argv)
 
         try {
           solve_steepest_descend(ma, va, epsilon);
+          solve_conjugate_gradient(ma, va, epsilon);
         } catch (const char *e) {
           cout << e << endl;
         }
